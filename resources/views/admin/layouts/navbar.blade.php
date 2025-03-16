4bc18fba4f887b129339dd917ecd2fa198d43d14
Akdleo <adityo256d@gmail.com>
2025-03-16 10:32:51 +0600

    update

--- resources/views/admin/layouts/navbar.blade.php
+++ resources/views/admin/layouts/navbar.blade.php
@@ -82,8 +82,8 @@
                       <a class="py-0 nav-link d-flex align-items-center" href="#" id="navbarDropdown"
                           role="button" data-bs-toggle="dropdown" aria-expanded="false">
                           <div class="caption ms-3 d-none d-md-block ">
-                              <h6 class="mb-0 caption-title">Mr. test</h6>
-                              <p class="mb-0 caption-sub-title">Admin</p>
+                              <h6 class="mb-0 caption-title">Mr. Admin</h6>
+                              
                           </div>
                       </a>
                       <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
